7ab186cf9104282591a598c9c524f95953e8b830
KonmaKanSinPack <saierpipi@gmail.com>
2025-12-20 16:07:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from astrbot.core.utils.session_waiter import (
     session_waiter,
     SessionController,
 )
-import asyncio
+# import asyncio
 from astrbot.api.event import MessageChain
 from astrbot.api import AstrBotConfig
 @register("astrbot_plugin_betterchat", "兔子", "更好的聊天。", "v1.0.0")
@@ -18,9 +18,9 @@ class Chat4severals_Plugin(Star):
         self.is_listening = False
         self.hole_msgs = ""
         self.iswaitting = False
-        self._ready_event = asyncio.Event()
+        # self._ready_event = asyncio.Event()
         self.config = config
-        self.timer = self.config.get("timer", 4.0)
+        
 
     async def initialize(self):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
@@ -31,20 +31,21 @@ class Chat4severals_Plugin(Star):
             logger.info("当前正在监听消息，请稍后再试。")
             return
         # umo = event.unified_msg_origin
+        timer = self.config.get("timer", 4.0)
         self.is_listening = True
         try:
-            @session_waiter(timeout=self.timer, record_history_chains=False)
+            @session_waiter(timeout=timer, record_history_chains=False)
             async def wait_for_response(controller: SessionController, event: AstrMessageEvent):
                 cur_msg = event.message_str
                 self.hole_msgs += f"{cur_msg}\n"
-                controller.keep(timeout=self.timer, reset_timeout=True)
+                controller.keep(timeout=timer, reset_timeout=True)
             try:
                 await wait_for_response(event)
             except TimeoutError:
                 logger.info("No more messages received within timeout.")
                 logger.info(f"Collected messages:{self.hole_msgs}")
                 # message_chain = MessageChain().message(self.hole_msgs)
-                self._ready_event.set()
+                # self._ready_event.set()
                 self.is_listening = False
                 event.message_str=self.hole_msgs
             except Exception as e:
